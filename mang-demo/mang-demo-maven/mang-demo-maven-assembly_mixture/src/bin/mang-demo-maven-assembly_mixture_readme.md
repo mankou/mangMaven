8fa0412a624f2c3c9b40ae6b95c2d31f23514a9e
mang <ningma003@gmail.com>
2016-08-19 13:19:56 +0800

    + 又增加了2个工程

---
+++ mang-demo/mang-demo-maven/mang-demo-maven-assembly_mixture/src/bin/mang-demo-maven-assembly_mixture_readme.md
@@ -0,0 +1,4 @@
+﻿# 说明
+ * 该工程是个失败的例子
+ * 我本想整合 mang-demo-maven-assembly 和 mang-demo-maven-assembly_withDependeyncy 但失败了
+ * 想既能打成zip的分发包,但lib下只有一个包含所有依赖的jar包 目前不知道怎么做我的尝试都不对
---
+++ mang-demo/mang-demo-maven/mang-demo-maven-shade/src/main/java/mang/demo/maven/shade/App.java
@@ -0,0 +1,13 @@
+package mang.demo.maven.shade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
