ce2465db7b2db66d6fb14a26ca4cf87f37a16340
mang <ningma003@gmail.com>
2016-10-14 09:48:06 +0800

    + 增加RandomUtil.java 随机字符串工具类 * EntityUtil.java 增加setEntityValue()方法

--- mang-util/src/main/java/mang/util/common/EntityUtil.java
+++ mang-util/src/main/java/mang/util/common/EntityUtil.java
@@ -4,10 +4,13 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 /**
  * 实体类工具类.
  * */
 public class EntityUtil {
+	private static Logger logger=Logger.getLogger(EntityUtil.class);
 
 	/**
 	 * 获取两个实例相同的属性名.
@@ -32,5 +35,37 @@ public class EntityUtil {
 		String[] samePropertyArray=(String[])samePropertyList.toArray(new String[0]);
 		return samePropertyArray;
 		
+	}
+	
+	/**
+	 * 给一个实体的某一属性设置属性值.
+	 * 
+	 * @param obj 实体
+	 * @param propertyName 属性名
+	 * @param value 属性值
+	 * */
+	public static void setEntityValue(Object obj,String propertyName,Object value){
+		try{
+			Field srcField = obj.getClass().getDeclaredField(propertyName);
+			srcField.setAccessible(true); //设置些属性是可以访问的  因有时属性是private的 如果不设置会报错
+			srcField.set(obj, value);
+		}catch (Exception e) {
+			logger.warn("属性不存在:"+propertyName);
+		}
+	}
+	
+	/**
+	 * 给一个实体的一组属性设置属性值.
+	 * 
+	 * @param obj 实体
+	 * @param propertyNameArr 属性名
+	 * @param valueList 属性值 注属性值要与属性名一一对应
+	 * */
+	public static void setEntityValue(Object obj,String[] propertyNameArr,List<Object> valueList){
+		for(int i=0;i<propertyNameArr.length;i++){
+			String propertyName=propertyNameArr[i];
+			Object value=valueList.get(i);
+			setEntityValue(obj, propertyName, value);
+		}
 	}
 }
